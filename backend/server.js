01cb84bc3c830812f5c123ea438edbde212c5450
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-17 21:46:09 +0530

    env port added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(CardRoutes.routes())
     .use(CardRoutes.allowedMethods())
 
 
-app.listen(3001,(err)=>{
+const port = process.env.PORT || 5000;
+
+app.listen(port,(err)=>{
 
       if(err){
          console.log(err);
